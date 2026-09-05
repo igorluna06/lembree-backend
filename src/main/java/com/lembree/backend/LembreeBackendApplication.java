8633c3b5661868b0e7606a18bed1a5a7b98b76c9
Igor Luna <igorluna2006@gmail.com>
2026-09-05 10:21:52 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/lembree/backend/LembreeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lembree.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LembreeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LembreeBackendApplication.class, args);
+	}
+
+}
